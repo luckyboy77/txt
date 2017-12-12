8e29a4eee8c0911706bf44b091ad4bc0fd6f529d
xupf <xupengfei151369@126.com>
2017-12-12 22:53:28 +0800

    就是房卡放技能

---
+++ 40 工具类/二维码生成所需jar包/OSSClientConstants.java
@@ -0,0 +1,20 @@
+package com.system.utils;
+/** 
+ * @ClassName: OSSClientConstants 
+ * @Description:  
+ * @author xupf 
+ * @date 2017年12月12日 下午10:22:40 
+ */
+public class OSSClientConstants {
+
+	//阿里云API的外网域名  
+    public static final String ENDPOINT = "oss-cn-shanghai.aliyuncs.com";  
+    //阿里云API的密钥Access Key ID  
+    public static final String ACCESS_KEY_ID = "LTAIjUP8xNlUEUlI";  
+    //阿里云API的密钥Access Key Secret  
+    public static final String ACCESS_KEY_SECRET = "zetFkxwsh6O5Orm11nAlvfQ3HY7Un9";  
+    //阿里云API的bucket名称  
+    public static final String BACKET_NAME = "wangdewangfei";  
+    //阿里云API的文件夹名称  
+    public static final String FOLDER="guide/";
+}
